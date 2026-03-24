e73fd80f5e9a4cfb2b0151114e6f0d0385c55e3e
Aisha <vetersevernyi13@gmail.com>
2026-03-24 18:29:02 +0300

    Basic array methods

---
+++ homework-7.js
@@ -0,0 +1,102 @@
+import {comments} from './comments.js'
+
+//1. Создаем массив из чисел 1-10
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const filteredNumbers = numbers.filter(num => num >= 5);
+console.log(filteredNumbers);
+
+
+//2. Создаем массив строк, относящихся к любой сущности
+
+const educationalSubjects = [
+  'Математика',
+  'Биология',
+  'География',
+  'Физика',
+  'Химия',
+  'История'
+]
+const hasMath = educationalSubjects.includes('Математика');
+console.log(hasMath);
+
+
+//3. Пишем функцию, которая будет применять массив и изменять его порядок напротивоположный
+
+function reverseMyData(anyArray) {
+  return anyArray.reverse();
+}
+
+// Используем ее для 1го задания
+
+const reversedNumbers = reverseMyData(filteredNumbers);
+console.log(reversedNumbers);
+
+// Используем ее для 2го задания
+
+const reversedSubject = reverseMyData(educationalSubjects);
+console.log(reversedSubject);
+
+
+
+//Уровень 2.
+//1. Фильтруем объекты массива, почта которых содержит 'com'
+
+const comEmails = comments.filter(item => item.email.includes('.com'));
+console.log(comEmails);
+
+
+//2. Перебираем массив 
+
+const updatedComments = comments.map(item => {
+  return {
+    ...item,
+    postId: item.id <= 5 ? 2 : 1
+  };
+});
+
+
+//3. Перебираем массив, оставляем только id и имя
+
+const shortComments = comments.map(item => {
+  return {
+    id: item.id,
+    name: item.name
+  };
+});
+
+
+//4. Перебираем массив и добавляем свойство isInvalid
+
+const validatedComments = comments.map(item => {
+  return {
+    ...item,
+    isInvalid: item.body.lenght > 180
+  };
+});
+
+console.log(validatedComments);
+
+
+//Уровень 3.
+//1. Используем массив reduce
+
+// используем map
+
+const emailsMap = comments.map(item => item.email);
+
+
+// используем reduce
+
+const emailReduce = comments.reduce((acc, item) => {
+  acc.push(item.email);
+  return acc;
+}, []);
+
+
+//2. Используем toString(), join()
+
+const string1 = emailsMap.toString();
+
+const string2 = emailsMap.join(', ');
+
